d57ec17a29520600f72c44693aad3b2760d56158
RohitashYogi <rohitash.yogi@probusinsurance.com>
2022-03-30 17:18:05 +0530

    Library Version 1.024

---
+++ ProbusDesign/src/main/java/in/probusinsurance/probusdesign/UI/TextView/TextViewReviewHeading.kt
@@ -0,0 +1,15 @@
+package `in`.probusinsurance.probusdesign.UI.TextView
+
+import `in`.probusinsurance.probusdesign.R
+import android.annotation.SuppressLint
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.TextView
+
+@SuppressLint("NewApi")
+class TextViewReviewHeading(context: Context, attrs: AttributeSet?) :
+    androidx.appcompat.widget.AppCompatTextView(context, attrs) {
+        init {
+            this.setTextAppearance(R.style.TextView_ReviewHeading_Style)
+        }
+}
