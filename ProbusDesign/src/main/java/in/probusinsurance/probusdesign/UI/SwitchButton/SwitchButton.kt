e8cb24f537b9f86b478f238cb4506b291a267097
RohitashYogi <rohitash.yogi@probusinsurance.com>
2022-09-26 17:07:39 +0530

    Version 1.051 Switch Button Working

--- ProbusDesign/src/main/java/in/probusinsurance/probusdesign/UI/SwitchButton/SwitchButton.kt
+++ ProbusDesign/src/main/java/in/probusinsurance/probusdesign/UI/SwitchButton/SwitchButton.kt
@@ -1,8 +1,12 @@
 package `in`.probusinsurance.probusdesign.UI.SwitchButton
 
+import `in`.probusinsurance.probusdesign.R
 import android.content.Context
 import android.util.AttributeSet
 
 class SwitchButton(context: Context, attrs: AttributeSet?) : androidx.appcompat.widget.SwitchCompat(context, attrs) {
 
+    init {
+        this.setTextAppearance(context, R.style.RadioButton_Gender_Style)
+    }
 }
