c07c069e15177ba87e9d9ee2072a805c2432ff78
RohitashYogi <rohitash.yogi@probusinsurance.com>
2022-11-30 12:09:30 +0530

    Data Binding Works

--- app/src/main/java/in/probusinsurance/probusfrontend/BindingTestActivity.kt
+++ app/src/main/java/in/probusinsurance/probusfrontend/BindingTestActivity.kt
@@ -1,10 +1,12 @@
 package `in`.probusinsurance.probusfrontend
 
+import `in`.probusinsurance.probusdesign.MasterEntity
 import `in`.probusinsurance.probusfrontend.databinding.ActivityBindingTestBinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 
 class BindingTestActivity : AppCompatActivity() {
 
@@ -25,5 +27,30 @@ class BindingTestActivity : AppCompatActivity() {
             Toast.makeText(applicationContext,"Lib Back Button",Toast.LENGTH_SHORT).show()
 
         }
+
+        var list = arrayListOf<MasterEntity>()
+        list.add(MasterEntity("Select Education *", "Select Education *"))
+        list.add(MasterEntity("12", "12 th Pass"))
+        list.add(MasterEntity("10", "10 th Pass"))
+        list.add(MasterEntity("15", "Graduation"))
+        list.add(MasterEntity("18 ", "Post Graduation"))
+        binding.spinner.setSpinner(this, list)
+       // spinner.autoFillByName("Graduation")
+        // spinner.autoFillById("10")
+        // spinner.autoFillByMaster(MasterEntity("18 ", "Post Graduation"))
+
+        binding.spinner.selectedItem.observe(this, Observer {
+            Toast.makeText(
+                applicationContext,
+                it.Name + " : " + it.Id,
+                Toast.LENGTH_SHORT
+            ).show()
+        })
+//        binding.spinner.setSpinner(
+//            this,
+//            resources.getStringArray(`in`.probusinsurance.probusdesign.R.array.Idarray_yesno),
+//            resources.getStringArray(`in`.probusinsurance.probusdesign.R.array.array_yesno)
+//        )
+
     }
 }
